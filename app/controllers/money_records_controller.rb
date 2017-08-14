970d63ded6b1d2c4318cf66e68ccba5e753a4c9d
Ivan Dymkov <mstrdymio@gmail.com>
2017-08-14 18:41:49 +0700

    Restrict user access to money sector

--- app/controllers/money_records_controller.rb
+++ app/controllers/money_records_controller.rb
@@ -1,5 +1,5 @@
 class MoneyRecordsController < ApplicationController
-  before_action :authenticate_user!
+  before_action :authenticate_user!, :check_user_is_able_to_see_money
 
   def index
     @records = MoneyRecord.ordered
@@ -19,9 +19,23 @@ class MoneyRecordsController < ApplicationController
   private
 
   def money_record_params
-      params.require(:money_record).permit(
-        :moment, :amount, :kind,
-        :category_id, :nakama_id, :project_id,
-        :comment)
+    params.require(:money_record).permit(
+      :moment, :amount, :kind,
+      :category_id, :nakama_id, :project_id,
+      :comment)
+  end
+
+  def check_user_is_able_to_see_money
+    unless current_user.moneyman?
+      respond_to do |format|
+        format.html do
+          flash[:alert] = 'Access denied'
+          redirect_to root_url
+        end
+        format.json do
+          render json: { error: 'Access denied' }, status: :unauthorized
+        end
+      end
     end
+  end
 end
