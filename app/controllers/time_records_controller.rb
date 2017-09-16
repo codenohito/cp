cffc46d880065c76de02c7ef30126304e5d2bee4
Ivan Dymkov <mstrdymio@gmail.com>
2017-09-16 09:15:12 +0700

    Time reports correct work

--- app/controllers/time_records_controller.rb
+++ app/controllers/time_records_controller.rb
@@ -2,26 +2,86 @@ class TimeRecordsController < ApplicationController
   before_action :authenticate_user!
 
   def index
-    @avl_nakamas = Nakama.accessible_by(current_ability, :read).ordered
-    @avl_projects = Project.accessible_by(current_ability, :read).ordered
+    build_base_report_with_filter
 
+    # = = = Filter = = = = = = = = = = = = = = = = = = = = = = = = = = = = = =
+    @filter[:from_day] = nil
+    @filter[:to_day] = nil
     today = Date.today
-    @filter = {
-      from_day: today.beginning_of_month,
-      to_day: today.end_of_month,
-      project_ids: [],
-      nakama_ids: []
-    }
+
+    if params[:from_day].present?
+      begin
+        @filter[:from_day] = Date.parse(params[:from_day])
+      rescue
+        # do nothing
+      end
+    end
+    @filter[:from_day] = today.beginning_of_month if @filter[:from_day].nil?
+
+    if params[:to_day].present?
+      begin
+        @filter[:to_day] = Date.parse(params[:to_day])
+      rescue
+        # do nothing
+      end
+    end
+    @filter[:to_day] = today.end_of_month if @filter[:to_day].nil?
+
+    @filter[:to_day] = @filter[:from_day] if @filter[:to_day] < @filter[:from_day]
+
+    # = = = Data = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = =
+    @records = @records.
+      where('time_records.theday >= ? AND time_records.theday <= ?',
+            @filter[:from_day],
+            @filter[:to_day])
+
+    @sums_by_nakama_projects = []
+    @avl_nakamas.each { |nkm| @sums_by_nakama_projects[nkm.id] = [] }
+    @records.each do |record|
+      csum = @sums_by_nakama_projects[record.nakama_id][record.project_id] || 0
+      @sums_by_nakama_projects[record.nakama_id][record.project_id] = csum + record.amount
+    end
   end
 
   def weekly_report
+    build_base_report_with_filter
+
+    # = = = Filter = = = = = = = = = = = = = = = = = = = = = = = = = = = = = =
     the_day_in_the_week = Date.today
-    @nakamas = Nakama.accessible_by(current_ability, :read).ordered
-    @projects = Project.accessible_by(current_ability, :read).ordered
+    @filter[:weeks_before] = 0
+
     if params[:weeks_before].present?
+      @filter[:weeks_before] = params[:weeks_before]
       the_day_in_the_week = the_day_in_the_week - params[:weeks_before].to_i.weeks
     end
+
     @week_start = the_day_in_the_week.beginning_of_week
+
+    # = = = Data = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = =
+    @records = @records.where('theday >= ? AND theday <= ?',
+                              @week_start,
+                              @week_start + 6.days)
+
+    @sums_by_nakamas = []
+    @avl_nakamas.each { |nkm| @sums_by_nakamas[nkm.id] = 0 }
+
+    @weekly_report_data = []
+
+    @records.each do |record|
+      @sums_by_nakamas[record.nakama_id] += record.amount
+      unless @weekly_report_data[record.project_id]
+        @weekly_report_data[record.project_id] = {}
+      end
+      daystr = record.theday.strftime "%Y%m%d"
+      unless @weekly_report_data[record.project_id][daystr]
+        @weekly_report_data[record.project_id][daystr] = []
+      end
+      if @weekly_report_data[record.project_id][daystr][record.nakama_id]
+        @weekly_report_data[record.project_id][daystr][record.nakama_id] += record.amount
+      else
+        @weekly_report_data[record.project_id][daystr][record.nakama_id] = record.amount
+      end
+    end
   end
 
   def create
@@ -110,5 +170,25 @@ class TimeRecordsController < ApplicationController
     if cannot?(:work_with_expired, @record) && (@record.theday <= Date.today - 1.week)
       @record.errors[:access] << 'You cannot manage time records older than week'
     end
+  end
+
+  def build_base_report_with_filter
+    @avl_nakamas = Nakama.accessible_by(current_ability, :read).ordered
+    @avl_projects = Project.accessible_by(current_ability, :read).ordered
+
+    # = = = Filter = = = = = = = = = = = = = = = = = = = = = = = = = = = = = =
+    @filter = { nakama_ids: [], project_ids: [] }
+    @filter[:project_ids] = params[:project_ids] if params[:project_ids].present?
+    @filter[:nakama_ids] = params[:nakama_ids] if params[:nakama_ids].present?
+
+    # = = = Data = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = =
+    rec_nkms = @avl_nakamas.map { |nkm| nkm.id.to_s }
+    rec_nkms = rec_nkms & @filter[:nakama_ids] if @filter[:nakama_ids].any?
+    rec_prjs = @avl_projects.map { |prj| prj.id.to_s }
+    rec_prjs = rec_prjs & @filter[:project_ids] if @filter[:project_ids].any?
+    @records = TimeRecord.includes(:project, :nakama).
+      where(nakama_id: rec_nkms).
+      where(project_id: rec_prjs).
+      ordered
   end
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,7 +1,6 @@
 module ApplicationHelper
 
   def formatted_worktime(time_in_minutes = 0)
-    p time_in_minutes
     "#{time_in_minutes / 60}:" + "%02d" % (time_in_minutes % 60)
   end
 
