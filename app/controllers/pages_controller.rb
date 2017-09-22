3bf003c2514abfdd13ad9d620e5e683c773321ee
Ivan Dymkov <mstrdymio@gmail.com>
2017-09-22 20:08:58 +0700

    Add active timers ans yesterday sum to dashboard

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,10 +2,6 @@ class PagesController < ApplicationController
   before_action :authenticate_user!
 
   def dashboard
-    if current_user.admin?
-      @nakamas = Nakama.order(id: :asc)
-    else
-      @nakamas = [current_nakama]
-    end
+    @nakamas = Nakama.accessible_by(current_ability, :read).ordered
   end
 end
